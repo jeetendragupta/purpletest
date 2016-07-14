11e522d7f56c1dd39929c5db8136154c9346ee28
jeetendragupta <jeetendra.gupta@purplecommerce.com>
2016-07-14 16:31:38 +0530

    14072016 04:31 PM

--- index2.php
+++ index2.php
@@ -1,16 +1,4 @@
 <?php 
 echo 'line before 1111';
-echo 'line 1 but ';
-echo 'line 2';
-echo 'line 2';
-echo 'line 2';
-echo 'line 2';
-echo 'line 2';
-echo 'line 2';
-echo 'line 2';
-echo 'line 2';
-echo 'line 2';
-echo 'line 2';
-echo 'line 1111';
 ?>
  
