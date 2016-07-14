3927c966b60d53c3b4ae94c68f0d41ff59ac98c1
jeetendragupta <jeetendra.gupta@purplecommerce.com>
2016-07-14 14:41:46 +0530

    14072016 02:41 PM

--- index2.php
+++ index2.php
@@ -1,4 +1,5 @@
 <?php 
-echo 'hello all this is index2.php file created by github create new file.';
-echo 'hello all this is index2.php file created by github create new file.';
+echo 'line 1';
+echo 'line 2';
+echo 'line 3';
 ?>
